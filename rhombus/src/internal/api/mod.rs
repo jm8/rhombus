7ff6694a2e7797237882556c7b9e6349d9111cec
Joshua Sims <jm8hjkl@gmail.com>
2024-10-03 00:13:57 -0400

    Challenges public api

--- rhombus/src/internal/api/mod.rs
+++ rhombus/src/internal/api/mod.rs
@@ -1,39 +1,22 @@
-use super::{auth::MaybeUser, router::RouterState};
+mod challenges;
+
+use super::router::RouterState;
 use aide::{
     axum::{routing::get, IntoApiResponse},
     openapi::{Info, OpenApi},
     redoc::Redoc,
 };
-use axum::{extract::State, Extension, Json};
-use schemars::JsonSchema;
-use serde::Serialize;
+use axum::{Extension, Json};
+use challenges::get_challenges;
 
 async fn serve_api(Extension(api): Extension<OpenApi>) -> impl IntoApiResponse {
     Json(api)
 }
 
-#[derive(Serialize, JsonSchema)]
-struct Challenge {
-    name: String,
-}
-
-async fn challenges_route(state: State<RouterState>) -> impl IntoApiResponse {
-    let challenges = state.db.get_challenges().await.unwrap();
-    Json(
-        challenges
-            .challenges
-            .iter()
-            .map(|chal| Challenge {
-                name: chal.name.clone(),
-            })
-            .collect::<Vec<_>>(),
-    )
-}
-
 pub fn build_api_router() -> axum::Router<RouterState> {
     let router = aide::axum::ApiRouter::new()
         .api_route("/", get(|| async { "Hello!".to_string() }))
-        .api_route("/challenges", get(challenges_route))
+        .api_route("/challenges", get(get_challenges))
         .route("/openapi.json", get(serve_api))
         .route("/docs", Redoc::new("/api/v1/openapi.json").axum_route());
 
