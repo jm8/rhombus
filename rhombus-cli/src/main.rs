47266aefd691a93330bda6d9de9fef11b7108133
Joshua Sims <jm8hjkl@gmail.com>
2024-10-09 18:49:36 -0400

    temp

--- rhombus-cli/src/main.rs
+++ rhombus-cli/src/main.rs
@@ -1,71 +1,77 @@
 use clap::{Parser, Subcommand};
+use config::{Config, ConfigError, FileFormat};
 use rhombus_api_client::apis::{
     configuration::{self, Configuration},
     default_api::challenges_get,
 };
+use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum RhombusCliError {
+    #[error("error loading challenges config")]
+    ConfigError(#[from] ConfigError),
+}
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    /// Optional name to operate on
-    name: Option<String>,
-
-    /// Sets a custom config file
-    #[arg(short, long, value_name = "FILE")]
-    config: Option<PathBuf>,
-
-    /// Turn debugging information on
-    #[arg(short, long, action = clap::ArgAction::Count)]
-    debug: u8,
-
     #[command(subcommand)]
-    command: Option<Commands>,
+    command: Commands,
 }
 
 #[derive(Subcommand)]
 enum Commands {
-    /// does testing things
-    Test {
-        /// lists test values
-        #[arg(short, long)]
-        list: bool,
+    Challenge {
+        #[command(subcommand)]
+        command: ChallengeCommands,
     },
+}
+
+#[derive(Subcommand)]
+enum ChallengeCommands {
+    Sync,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Category {
+    pub stable_id: Option<String>,
+    pub name: String,
+    pub color: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Author {
+    pub stable_id: Option<String>,
+    pub name: String,
+    pub avatar: String,
+    pub discord_id: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct ChallengeLoaderConfiguration {
+    pub categories: Vec<Category>,
+    pub authors: Vec<Author>,
 }
 
 #[tokio::main]
 async fn main() {
     let cli = Cli::parse();
 
-    // You can check the value provided by positional arguments, or option arguments
-    if let Some(name) = cli.name.as_deref() {
-        println!("Value for name: {name}");
-    }
+    let config = Config::builder()
+        .add_source(config::File::new("loader.yaml", FileFormat::Yaml))
+        .build()
+        .unwrap()
+        .try_deserialize::<ChallengeLoaderConfiguration>()
+        .unwrap();
 
-    if let Some(config_path) = cli.config.as_deref() {
-        println!("Value for config: {}", config_path.display());
-    }
+    eprintln!("{:#?}", config);
 
-    // You can see how many times a particular flag or argument occurred
-    // Note, only flags can have multiple occurrences
-    match cli.debug {
-        0 => println!("Debug mode is off"),
-        1 => println!("Debug mode is kind of on"),
-        2 => println!("Debug mode is on"),
-        _ => println!("Don't be crazy"),
-    }
-
-    // You can check for the existence of subcommands, and if found use their
-    // matches just as you would the top level cmd
-    match &cli.command {
-        Some(Commands::Test { list }) => {
-            if *list {
-                println!("Printing testing lists...");
-            } else {
-                println!("Not printing testing lists...");
-            }
-        }
-        None => {}
+    match cli.command {
+        Commands::Challenge {
+            command: ChallengeCommands::Sync,
+        } => {}
     }
 
     let configuration = Configuration {
