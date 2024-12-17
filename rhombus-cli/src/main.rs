a94216aa65b48dbfa1b0309fb0aadbda0e03a9b9
Joshua Sims <jm8hjkl@gmail.com>
2024-12-17 14:33:39 -0500

    Load yaml with figment

--- rhombus-cli/src/main.rs
+++ rhombus-cli/src/main.rs
@@ -1,7 +1,15 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use clap::{Parser, Subcommand};
+use figment::{
+    providers::{Format, Yaml},
+    Figment,
+};
 use grpc::proto::{rhombus_client::RhombusClient, HelloRequest};
-use std::path::PathBuf;
+use serde::{Deserialize, Serialize};
+use std::{
+    fs::{self, ReadDir},
+    path::{Path, PathBuf},
+};
 
 mod grpc {
     pub mod proto {
@@ -37,6 +45,85 @@ enum Commands {
     },
 }
 
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+struct LoaderYaml {
+    pub authors: Vec<AuthorYaml>,
+    pub categories: Vec<CategoryYaml>,
+}
+#[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
+struct AuthorYaml {
+    pub stable_id: String,
+    pub name: Option<String>,
+    pub avatar: String,
+    pub discord_id: u64,
+}
+
+#[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
+struct CategoryYaml {
+    pub stable_id: String,
+    pub name: Option<String>,
+    pub color: String,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+struct ChallengeYaml {
+    pub stable_id: String,
+    pub author: String,
+    pub category: String,
+    pub description: String,
+    pub files: Vec<ChallengeAttachmentYaml>,
+    pub flag: String,
+    pub healthscript: Option<String>,
+    pub name: Option<String>,
+    pub ticket_template: Option<String>,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+enum ChallengeAttachmentYaml {
+    Url { url: String, dst: String },
+    File { src: String, dst: String },
+}
+
+struct ChallengeYamlWalker {
+    stack: Vec<ReadDir>,
+}
+
+impl ChallengeYamlWalker {
+    fn new(root: &Path) -> Self {
+        let mut stack = Vec::new();
+        if root.is_dir() {
+            stack.push(fs::read_dir(root).unwrap());
+        }
+        ChallengeYamlWalker { stack }
+    }
+}
+
+impl Iterator for ChallengeYamlWalker {
+    type Item = PathBuf;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some(dir_iter) = self.stack.last_mut() {
+            if let Some(entry) = dir_iter.next() {
+                match entry {
+                    Ok(entry) => {
+                        let path = entry.path();
+                        if path.is_dir() {
+                            self.stack.push(fs::read_dir(path).unwrap());
+                        } else if path.is_file() && path.file_name().unwrap() == "challenge.yaml" {
+                            return Some(path);
+                        }
+                    }
+                    Err(_) => continue,
+                }
+            } else {
+                self.stack.pop();
+            }
+        }
+        None
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let mut client = RhombusClient::connect("http://[::0]:3001").await?;
@@ -45,6 +132,22 @@ async fn main() -> Result<()> {
     });
     let response = client.say_hello(request).await?;
     println!("{:?}", response);
+
+    let config: LoaderYaml = Figment::new()
+        .merge(Yaml::file_exact("loader.yaml"))
+        .extract()?;
+    println!("{:#?}", config);
+
+    let challenge_yamls = ChallengeYamlWalker::new(&PathBuf::from("."))
+        .into_iter()
+        .map(|p| {
+            Figment::new()
+                .merge(Yaml::file_exact(&p))
+                .extract::<ChallengeYaml>()
+                .with_context(|| format!("failed to load {}", p.display()))
+        })
+        .collect::<Result<Vec<_>>>()?;
+    println!("{:#?}", challenge_yamls);
 
     Ok(())
 }
