38b384efa6fb3369df802e7909d7715f03bfd536
Joshua Sims <jm8hjkl@gmail.com>
2024-12-17 13:26:42 -0500

    Add grpc to rhombus-cli

--- rhombus-cli/src/main.rs
+++ rhombus-cli/src/main.rs
@@ -1,6 +1,13 @@
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+use grpc::proto::{rhombus_client::RhombusClient, HelloRequest};
 use std::path::PathBuf;
 
-use clap::{Parser, Subcommand};
+mod grpc {
+    pub mod proto {
+        tonic::include_proto!("rhombus");
+    }
+}
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -30,39 +37,14 @@ enum Commands {
     },
 }
 
-fn main() {
-    let cli = Cli::parse();
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut client = RhombusClient::connect("http://[::0]:3001").await?;
+    let request = tonic::Request::new(HelloRequest {
+        name: "Tonic".into(),
+    });
+    let response = client.say_hello(request).await?;
+    println!("{:?}", response);
 
-    // You can check the value provided by positional arguments, or option arguments
-    if let Some(name) = cli.name.as_deref() {
-        println!("Value for name: {name}");
-    }
-
-    if let Some(config_path) = cli.config.as_deref() {
-        println!("Value for config: {}", config_path.display());
-    }
-
-    // You can see how many times a particular flag or argument occurred
-    // Note, only flags can have multiple occurrences
-    match cli.debug {
-        0 => println!("Debug mode is off"),
-        1 => println!("Debug mode is kind of on"),
-        2 => println!("Debug mode is on"),
-        _ => println!("Don't be crazy"),
-    }
-
-    // You can check for the existence of subcommands, and if found use their
-    // matches just as you would the top level cmd
-    match &cli.command {
-        Some(Commands::Test { list }) => {
-            if *list {
-                println!("Printing testing lists...");
-            } else {
-                println!("Not printing testing lists...");
-            }
-        }
-        None => {}
-    }
-
-    // Continued program logic goes here...
+    Ok(())
 }
