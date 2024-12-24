0c8d7643e9bc3c47a86855161fc2df853a73fe4b
Joshua Sims <jm8hjkl@gmail.com>
2024-12-23 22:58:04 -0500

    temp

--- rhombus/src/internal/challenge_diff.rs
+++ rhombus/src/internal/challenge_diff.rs
@@ -2,7 +2,8 @@ use std::collections::HashMap;
 use std::hash::Hash;
 
 use crate::grpc::proto::{
-    AuthorPatch, CategoryPatch, ChallengeData, ChallengeDataPatch, ChallengePatch,
+    AuthorPatch, CategoryPatch, Challenge, ChallengeAttachment, ChallengeData, ChallengeDataPatch,
+    ChallengePatch,
 };
 
 fn zip_maps<'a, K: Eq + Hash, V>(
@@ -17,6 +18,33 @@ fn zip_maps<'a, K: Eq + Hash, V>(
         )
 }
 
+#[derive(PartialEq)]
+struct ChallengeWithoutPoints<'a> {
+    pub name: &'a str,
+    pub description: &'a str,
+    pub category: &'a str,
+    pub author: &'a str,
+    pub ticket_template: Option<&'a str>,
+    pub files: &'a Vec<ChallengeAttachment>,
+    pub flag: &'a str,
+    pub healthscript: Option<&'a str>,
+    pub score_type: Option<&'a str>,
+}
+
+fn without_points<'a>(chal: &'a Challenge) -> ChallengeWithoutPoints<'a> {
+    ChallengeWithoutPoints {
+        name: &chal.name,
+        description: &chal.description,
+        category: &chal.category,
+        author: &chal.author,
+        ticket_template: chal.ticket_template.as_deref(),
+        files: &chal.files,
+        flag: &chal.flag,
+        healthscript: chal.healthscript.as_deref(),
+        score_type: chal.score_type.as_deref(),
+    }
+}
+
 pub fn diff_challenge_data(old: &ChallengeData, new: &ChallengeData) -> ChallengeDataPatch {
     ChallengeDataPatch {
         challenges: zip_maps(&old.challenges, &new.challenges)
@@ -29,6 +57,9 @@ pub fn diff_challenge_data(old: &ChallengeData, new: &ChallengeData) -> Challeng
                         new: new.cloned(),
                     },
                 )
+            })
+            .filter(|(_k, patch)| {
+                patch.old.as_ref().map(without_points) != patch.new.as_ref().map(without_points)
             })
             .collect(),
         categories: zip_maps(&old.categories, &new.categories)
